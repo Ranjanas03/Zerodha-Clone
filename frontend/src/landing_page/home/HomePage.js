332b1dd8873468955d141b6d5b8b3e4e2b1d75a4
RANJANA SINGH <contactranjana03@gmail.com>
2025-02-22 23:48:56 +0530

    Update HomePage.js

--- frontend/src/landing_page/home/HomePage.js
+++ frontend/src/landing_page/home/HomePage.js
@@ -1,1 +1,25 @@
+import React from "react";
+import Hero from "./Hero";
+import Awards from "./Awards";
+import Stats from "./Stats";
+import Pricing from "./Pricing";
+import Education from "./Education";
 
+import Openaccount from "../support/Openaccount";
+import Navbar from "../support/Navbar";
+import Footer from "../support/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Awards />
+      <Stats />
+      <Pricing />
+      <Education />
+      <Openaccount />
+    </>
+  );
+}
+
+export default HomePage;
