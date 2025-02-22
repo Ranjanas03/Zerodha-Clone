1701ddec34a4579f4d133d1836aef3a9b0dea468
RANJANA SINGH <contactranjana03@gmail.com>
2025-02-22 23:57:34 +0530

    Update CreateTicket.js

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,1 +1,104 @@
+import React from "react";
 
+function CreateTicket(){
+    return(
+        <div className="container mt-5">
+            <div className="row mt-5 mb-5">
+                <h1 className="fs-1 mb-4 text-muted">
+                    To create a ticket, select a relevant topic
+                </h1>
+                <div className="col-4 mt-5">
+                   <h3 className="mb-3"><i class="fa-solid fa-circle-plus"></i> Account Opening</h3>
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Getting started</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>online</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Offline</a>
+                    <br /> 
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Charges</a>
+                    <br /> 
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none" ,lineHeight:"2.5"}}>Company, Partnership & HUF</a>
+                    <br /> 
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none" ,lineHeight:"2.5"}}>Non Resident Indian (NRI)</a>
+                    <br />
+                </div>
+                <div className="col-4 mt-5">
+                   <h3 className="mb-3"><i class="fa-regular fa-user"></i> Your Zerodha Account</h3>
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Login credentials</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Your Profile</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Account modification</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>CMR & DP ID</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Nomination</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Transfer and conversion of shares</a>
+                    <br />
+                </div>
+                <div className="col-4 mt-5">
+                   <h3 className="mb-3"><i class="fa-solid fa-circle-plus"></i> Tradings and Market</h3>
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Trading FAQs</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Kite</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Margins</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Product and other types</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Corporate actions</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Kite features</a>
+                    <br />
+                    
+                </div>
+            </div>
+            <div className="row mt-5 mb-5">
+                <div className="col-4 mb-5">
+                   <h3 className="mb-3"><i class="fa-solid fa-circle-plus"></i> Funds</h3>
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Fund withdrawal</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Adding funds</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Adding bank accounts</a>
+                    <br /> 
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>eMandates</a>
+                    <br /> 
+                    
+                </div>
+                <div className="col-4 mb-5">
+                   <h3 className="mb-3"><i class="fa-regular fa-user"></i> Console</h3>
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>IPO</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Portfolio</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Funds Statement</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Profile</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Reports</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Referral Programs</a>
+                    <br />
+                </div>
+                <div className="col-4 mb-5">
+                   <h4 className="mb-3"><i class="fa-solid fa-circle-plus"></i> Coin</h4>
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>Understanding mutual funds</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>COine app</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>web app</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>transactions and reports</a>
+                    <br />
+                    <a href="" className="fs-5 p-5" style={{textDecoration:"none", lineHeight:"2.5"}}>National Prison Scheme(NPS)</a>
+                    <br />
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default CreateTicket;
