f0bbce81a9f072863d45ceda6505ba6700330b06
RANJANA SINGH <contactranjana03@gmail.com>
2025-02-23 16:09:25 +0530

    Update Home.js

--- dashboard/src/Components/Home.js
+++ dashboard/src/Components/Home.js
@@ -1,1 +1,14 @@
+import React from "react";
 
+import Dashboard from "./Dashboard";
+import Topbar from "./Topbar";
+
+const Home = () =>{
+    return (
+        <>
+            <Topbar/>
+            <Dashboard/>
+        </>
+    );
+};
+export default Home;
