c2b5449945300206fea1984cc95848b195c44a33
RANJANA SINGH <contactranjana03@gmail.com>
2025-02-23 13:07:41 +0530

    Update OrdersModel.js

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -1,1 +1,5 @@
+const {model} = require("mongoose");
+const {OrdersSchema} = require('../schema/OrdersSchema');
 
+const OrdersModel = new model("order", OrdersSchema);
+module.exports = {OrdersModel};
