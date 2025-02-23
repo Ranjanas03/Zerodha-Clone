5e732a79eea635bb6f34284cbae07e5941392d7e
RANJANA SINGH <contactranjana03@gmail.com>
2025-02-23 13:07:00 +0530

    Update HoldingsModel.js

--- backend/model/HoldingsModel.js
+++ backend/model/HoldingsModel.js
@@ -1,1 +1,5 @@
+const {model} = require("mongoose");
+const {HoldingsSchema} = require('../schema/HoldingsSchema');
 
+const HoldingsModel = new model("holding", HoldingsSchema);
+module.exports = {HoldingsModel};
